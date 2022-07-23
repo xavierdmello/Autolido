ad2ef6a53109fb71822671e080bf1c62237aed18
Xavier DMello <xaviermdmello@gmail.com>
2022-07-23 18:25:40 -0400

    enabled borrowing

--- contracts/Autolido.sol
+++ contracts/Autolido.sol
@@ -4,10 +4,11 @@ import "./AnyswapV5ERC20.sol";
 import "./CErc20Interfaces.sol";
 import "./EIP20Interface.sol";
 import "./PriceOracle.sol";
+import "./ComptrollerInterface.sol";
 
 contract Autolido {
-    uint public safteyMargin; // 80% of LTV by default
-    uint public ltvDeviationThreshold = 5; // 5%    
+    uint256 public safteyMargin; // 80% of LTV by default
+    uint256 public ltvDeviationThreshold = 5; // 5%
     mapping(address => uint256) public balances; // number of collateralCTokens user owns in autolido
     address public owner;
 
@@ -26,82 +27,99 @@ contract Autolido {
 
         owner = msg.sender;
 
-        safteyMargin = 8*10**(collateralToken.decimals()-1); // 80% of LTV
+        safteyMargin = 8 * 10**(collateralToken.decimals() - 1); // 80% of LTV
 
         // Automatically enter market
         enterMarket();
     }
-    
+
     // Automatic way to deposit tokens. Will be called When approveAndCall() is called on the USDC contract.
     // This allows tokens to be approved and deposited in one transcation
-    function onTokenApproval(address owner, uint256 value, bytes memory data) external returns(bool){
+    function onTokenApproval(
+        address owner,
+        uint256 value,
+        bytes memory data
+    ) external returns (bool) {
         _deposit(owner, value);
         return true;
     }
 
     // Manual way to deposit tokens. Must approve tokens first if calling this function.
-    function deposit(uint value) public {
+    function deposit(uint256 value) public {
         _deposit(msg.sender, value);
     }
 
     // Internal deposit function
-    function _deposit(address owner, uint value) private {
+    function _deposit(address owner, uint256 value) private {
         // Deposit tokens into market
-        balances[owner] += (value*10**18)/collateralCToken.exchangeRateCurrent();
+        balances[owner] += (value * 10**18) / collateralCToken.exchangeRateCurrent();
         collateralToken.transferFrom(owner, address(this), value);
         collateralToken.approve(address(collateralCToken), value);
-        require(collateralCToken.mint(value)==0, "Minting error");    
+        require(collateralCToken.mint(value) == 0, "Minting error");
 
         // Borrow borrowToken
         // TODO: Make sure borrow won't push autolido into liquidation range before a borrow (it shouldn't, but hey, who knows?)
-        // require(borrowCToken.borrow(amountToBorrow(value))==0, "Borrow error");
+        // This will fail on a local testnet, because xcKSM is a XC-20 token which depends special on rust code in the moonriver node
+        require(borrowCToken.borrow(amountToBorrow(value)) == 0, "Borrow error");
     }
-    
+
     // Calculates safe amount to borrow in borrow token, given amount of collateral token
     // TODO: Compare gas price of calculating borrow amount using this function vs. using getAccountLiquidity() twice
-    function amountToBorrow(uint depositAmount) public view returns(uint) {
-        (,uint collateralFactorMantissa,) = collateralCToken.comptroller().markets(address(collateralCToken));
+    function amountToBorrow(uint256 depositAmount) public view returns (uint256) {
+        (, uint256 collateralFactorMantissa, ) = collateralCToken.comptroller().markets(
+            address(collateralCToken)
+        );
 
         // TODO: Compare gas cost of hardcoding borrowCToken.comptroller() and borrowCToken.comptroller().oracle() and calling those instead of reading them from the CToken contract.
         // This may reduce gas cost, but means that the comptroller will have to be manually updated if it changes. The comptroller *should* never change, but it *can*.
-        uint depositTokenPrice = collateralCToken.comptroller().oracle().getUnderlyingPrice(address(collateralCToken)); // Denominated in 36-(Decimals of Underlying) decimals
-        uint depositUSDvalue = (depositAmount*depositTokenPrice)/10**(36-collateralToken.decimals()); // Same decimals as underlying
+        uint256 depositTokenPrice = collateralCToken.comptroller().oracle().getUnderlyingPrice(
+            address(collateralCToken)
+        ); // Denominated in 36-(Decimals of Underlying) decimals
+        uint256 depositUSDvalue = (depositAmount * depositTokenPrice) / 10**(36 - collateralToken.decimals()); // Same decimals as underlying
 
-        uint maxBorrowUSD  = (depositUSDvalue*collateralFactorMantissa)/10**18;
-        uint safeBorrowUSD = (maxBorrowUSD*safteyMargin)/10**collateralToken.decimals(); // 80% of max borrow by default
+        uint256 maxBorrowUSD = (depositUSDvalue * collateralFactorMantissa) / 10**18;
+        uint256 safeBorrowUSD = (maxBorrowUSD * safteyMargin) / 10**collateralToken.decimals(); // 80% of max borrow by default
 
-        uint borrowTokenPrice = borrowCToken.comptroller().oracle().getUnderlyingPrice(address(borrowCToken)); // Denominated in 36-(Decimals of Underlying) decimals
-        return (safeBorrowUSD*10**(2*borrowToken.decimals()-collateralToken.decimals()))/borrowTokenPrice; // Denominated in decimals of borrow token
+        // This will fail on a local testnet, because xcKSM is a XC-20 token which depends special on rust code in the moonriver node
+        uint256 borrowTokenPrice = borrowCToken.comptroller().oracle().getUnderlyingPrice(
+            address(borrowCToken)
+        ); // Denominated in 36-(Decimals of Underlying) decimals
+        uint256 amountToBorrow = (safeBorrowUSD *
+            10**(2 * borrowToken.decimals() - collateralToken.decimals())) / borrowTokenPrice; // Denominated in decimals of borrow token
+        return amountToBorrow;
     }
 
-    modifier onlyOwner {
-        assert(owner==msg.sender);
+    modifier onlyOwner() {
+        assert(owner == msg.sender);
         _;
     }
 
     // !! DEV FUNCTION - MAY BE REMOVED LATER !!
-    function enterMarket() public onlyOwner returns (uint[] memory) {
+    function enterMarket() public onlyOwner returns (uint256[] memory) {
         address[] memory market = new address[](1);
         market[0] = address(collateralCToken);
-        require(collateralCToken.comptroller().enterMarkets(market)[0]==0, "Enter Market Failed");
+        require(collateralCToken.comptroller().enterMarkets(market)[0] == 0, "Enter Market Failed");
     }
 
     // !! DEV FUNCTION - MAY BE REMOVED LATER !!
-    function exitMarket() public onlyOwner returns (uint) {
-        require(collateralCToken.comptroller().exitMarket(address(collateralCToken))==0, "Exit Market Failed");
+    function exitMarket() public onlyOwner returns (uint256) {
+        require(
+            collateralCToken.comptroller().exitMarket(address(collateralCToken)) == 0,
+            "Exit Market Failed"
+        );
     }
 
     // !! DEV FUNCTION - MAY BE REMOVED LATER !!
     // redeemTokens: amount of collateralCTokens to redeem from Moonwell
     function withdraw(uint256 redeemTokens) public onlyOwner {
-        uint balanceBefore = collateralToken.balanceOf(address(this));
+        uint256 balanceBefore = collateralToken.balanceOf(address(this));
         require(redeemTokens <= balances[msg.sender], "Withdrawal amount exceeds balance");
 
         balances[msg.sender] -= redeemTokens;
 
-        require(collateralCToken.redeem(redeemTokens)==0, "Withdrawal error");
+        require(collateralCToken.redeem(redeemTokens) == 0, "Withdrawal error");
 
-        uint balanceAfter = collateralToken.balanceOf(address(this));
-        collateralToken.transfer(msg.sender, balanceAfter-balanceBefore);
+        uint256 balanceAfter = collateralToken.balanceOf(address(this));
+        collateralToken.transfer(msg.sender, balanceAfter - balanceBefore);
     }
 }
